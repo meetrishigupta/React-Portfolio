389e41863a1aa5a0271206dbcce35ac5694ce041
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-07-31 11:58:00 +0000

    designhover_bug

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -34,7 +34,7 @@ export default function Projects() {
                 />
                 <div
                   style={{ height: "100%" }}
-                  className="px-8 py-10 relative h-full z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100 transition-opacity duration-500"
+                  className="px-8 py-10 relative h-full w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100 transition-opacity duration-500"
                 >
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
